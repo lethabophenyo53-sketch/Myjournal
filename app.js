467c945d98b199048051d17e508e0923be1c8de5
lethabophenyo53-sketch <lethabophenyo53@gmail.com>
2026-04-07 16:00:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
+let pages = [];
 let current = 0;
 
-function getPages() {
-  return document.querySelectorAll(".page");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  pages = document.querySelectorAll(".page");
+});
 
 function openBook() {
   document.querySelector(".cover").style.display = "none";
   document.getElementById("book").style.display = "block";
-  showPage(0);
+
+  pages = document.querySelectorAll(".page"); // FORCE REFRESH
+  current = 0;
+  showPage(current);
 }
 
 function showPage(i) {
-  const pages = getPages();
-
-  if (!pages.length) {
-    console.log("No pages found!");
-    return;
-  }
-
   pages.forEach(p => p.classList.remove("active"));
 
   if (pages[i]) {
     pages[i].classList.add("active");
-    current = i;
   }
 }
 
 function next() {
-  const pages = getPages();
   if (current < pages.length - 1) {
     current++;
     showPage(current);
