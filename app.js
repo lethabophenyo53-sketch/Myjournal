7363e8eae90c85cca66cf3a67ac3b6d3e4be2304
lethabophenyo53-sketch <lethabophenyo53@gmail.com>
2026-04-07 16:39:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -33,3 +33,21 @@ function selectMood(el) {
   document.querySelectorAll(".mood span").forEach(s => s.style.transform = "scale(1)");
   el.style.transform = "scale(1.4)";
 }
+
+function moodRain(emoji) {
+  for (let i = 0; i < 25; i++) {
+    const el = document.createElement("div");
+    el.classList.add("fall");
+    el.innerText = emoji;
+
+    el.style.left = Math.random() * window.innerWidth + "px";
+    el.style.fontSize = (15 + Math.random() * 20) + "px";
+    el.style.animationDuration = (2 + Math.random() * 2) + "s";
+
+    document.body.appendChild(el);
+
+    setTimeout(() => {
+      el.remove();
+    }, 3000);
+  }
+}
